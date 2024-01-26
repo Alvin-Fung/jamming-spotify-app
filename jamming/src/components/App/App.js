f4a0aa3d8c52f7fab85264c25064b8a2b52c1e0d
Alvin-Fung <alvin_fung9@outlook.com>
2024-01-26 15:30:56 +0000

    Created hard coded objects for testing

--- jamming/src/components/App/App.js
+++ jamming/src/components/App/App.js
@@ -1,9 +1,18 @@
+import React, { useState } from 'react';
+
 import './App.css';
 import SearchBar from '../SearchBar/searchBar';
 import SearchResults from '../SearchResults/searchResults';
 import Playlist from '../Playlist/Playlist';
 
 function App() {
+
+  //Step 1: Create hardcoded array of track objects
+  const hardCodedTracks = [
+    { id: '1', name: 'Song 1', artist: 'Artist 1', album: 'Album 1' },
+    { id: '2', name: 'Song 2', artist: 'Artist 2', album: 'Album 2' }
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
