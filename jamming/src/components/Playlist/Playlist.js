cd3705241f39837b288614a9b4121c3cf86e61e2
Alvin-Fung <alvin_fung9@outlook.com>
2024-03-13 22:01:01 +0000

    Fixed the parsing error thanks to stack overflow
    However, it looks like my usage of onChange() isn't quite right within the H2 tag

--- jamming/src/components/Playlist/Playlist.js
+++ jamming/src/components/Playlist/Playlist.js
@@ -15,8 +15,8 @@ function Playlist(props) {
                         value={playlistName}
                         onChange={(e) => setPlaylistName(e.target.value)}
                     />
-                ) }
-                <h2 onClick={() => onChange(true)}>{playlistName}</h2>
+                ) : <h2 onClick={() => onChange(true)}>{playlistName}</h2>}
+
                 <Tracklist
                     tracks={props.playlistTracks}
                     onRemove={props.onRemove}
