20a6985e09846e2d0b5fc3479e9eb57078505bed
Alvin-Fung <alvin_fung9@outlook.com>
2024-03-13 14:45:40 +0000

    Used alert instead of console.log() and added a track that can be used for testing if there is an existing track within the playlist

--- jamming/src/components/App/App.js
+++ jamming/src/components/App/App.js
@@ -7,22 +7,28 @@ import Playlist from '../Playlist/Playlist';
 function App() {
   const [searchResults, setSearchResults] = useState([
     {
-      id: 4,
+      id: 1,
       name: "Plastic Love",
       artist: "Mariya Takeuchi",
       album: "Expressions"
     },
     {
-      id: 5,
+      id: 2,
       name: "Epitaph",
       artist: "King Crimson",
       album: "In The Court Of The Crimson King"
+    },
+    {
+      id: 3,
+      name: "Shine On You Crazy Diamond, PTs. 5-6",
+      artist: "Pink Floyd",
+      album: "Wish you Were Here"
     }
   ]);
   const [playlistName, setPlaylistName] = useState("My Playlist");
   const [playlistTracks, setPlaylistTracks] = useState([
     {
-      id: 1,
+      id: 3,
       name: "Shine On You Crazy Diamond, PTs. 5-6",
       artist: "Pink Floyd",
       album: "Wish you Were Here"
@@ -34,7 +40,7 @@ function App() {
     let isTrackInPlaylist = playlistTracks.some((t) => t.id === track.id);
 
     if (isTrackInPlaylist) {
-      console.log("Song is already in the playlist!");
+      alert("Song is already in the playlist!");
     } else {
       setPlaylistTracks((prevTracks) => [...prevTracks, track]);
     }
