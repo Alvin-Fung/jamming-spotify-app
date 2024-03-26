0e2c86438a023c1e3ba6f3ca822e12dd7e56c3e3
Alvin-Fung <alvin_fung9@outlook.com>
2024-03-26 16:26:57 +0000

    place holder and more comments

--- jamming/src/components/util/token.js
+++ jamming/src/components/util/token.js
@@ -1,12 +1,20 @@
 var client_id = 'CLIENT_ID';
 var redirect_uri = 'http://localhost:3000/callback';
 
-console.log();
-
 const Spotify = {
+    // Place holder for access token
+    accessToken: '',
+
+    // Method for access token retrieval
     getAccessToken() {
+        // Checks if it's set
         if (accessToken) {
             return accessToken;
+        } else {
+            // Otherwise, it will attempt to retrieve it from the URL
+            // Need some logic here that extracts the access token from the URL
+            // Probably also need to have a way of storing it and returning
+            // As well as handling for it if there is no token found
         }
     }
 }
