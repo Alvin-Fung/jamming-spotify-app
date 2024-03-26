ee693b20f0e16c3c7841819d786f0f0ac0da51b2
Alvin-Fung <alvin_fung9@outlook.com>
2024-03-26 18:14:54 +0000

    Found a way to shorten this to make code look neater with the url
    Also got rid of the 1st else statement because the content inside should just happen and not be an exception

--- jamming/src/components/Spotify/token.js
+++ jamming/src/components/Spotify/token.js
@@ -1,5 +1,6 @@
 var client_id = 'be14b06a1d904f64a78c523bedd02a33';
 var redirect_uri = 'http://localhost:3000/';
+var url = window.location.href;
 
 const Spotify = {
     // Place holder for access token
@@ -10,21 +11,20 @@ const Spotify = {
         // Checks if it's set
         if (accessToken) {
             return accessToken;
+        }
+        // Otherwise, it will attempt to retrieve it from the URL
+        const accessTokenMatch = url.match(/access_token=([^&]*)/);
+        const expiresInMatch = url.match(/expires_in=([^&]*)/);
+
+        // If they are both found in the URL, then it gets stored
+        if (accessTokenMatch && expiresInMatch) {
+            accessToken = accessTokenMatch[1];
+            const expiresIn = Number(expiresInMatch[1]);
+
+            window
+
         } else {
-            // Otherwise, it will attempt to retrieve it from the URL
-            const accessTokenMatch = window.location.href.match(/access_token=([^&]*)/);
-            const expiresInMatch = window.location.href.match(/expires_in=([^&]*)/);
-
-            // If they are both found in the URL, then it gets stored
-            if (accessTokenMatch && expiresInMatch) {
-                accessToken = accessTokenMatch[1];
-                const expiresIn = Number(expiresInMatch[1]);
-                // Probably also need to have a way of storing it and returning
-                // As well as handling for it if there is no token found
-
-            } else {
-                console.error('Access token was not found in URL');
-            }
+            console.error('Access token was not found in URL');
         }
     },
 };
