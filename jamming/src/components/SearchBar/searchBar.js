6ad4500ae9afb4e7c1085d71e82c1b3ffb2da70e
Alvin-Fung <alvin_fung9@outlook.com>
2024-04-04 17:25:10 +0100

    handleInputChange()
    Might change this to an arrow function instead

--- jamming/src/components/SearchBar/searchBar.js
+++ jamming/src/components/SearchBar/searchBar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function SearchBar() {
+function SearchBar(props) {
+    const [searchTerm, setSearchTerm] = useState('');
 
+    function handleInputChange(event) {
+        setSearchTerm(event.target.value);
+    }
     return (
         <div className="Search-Bar">
             <input placeholder="Enter A Song, Album or Artist" />
