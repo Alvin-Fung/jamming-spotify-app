2c72a7a819846300f333b6439aeb2bd56a6d6343
Alvin-Fung <alvin_fung9@outlook.com>
2024-04-04 17:30:16 +0100

    handleSubmit() and i've noticed that i might be able to access onSearch via this approach

--- jamming/src/components/SearchBar/searchBar.js
+++ jamming/src/components/SearchBar/searchBar.js
@@ -1,14 +1,21 @@
 import React, { useState } from 'react';
 
-const SearchBar = (props) => {
+const SearchBar = ({ onSearch }) => {
     const [searchTerm, setSearchTerm] = useState('');
 
     const handleInputChange = (event) => {
         setSearchTerm(event.target.value);
     }
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        onSearch(searchTerm);
+    }
+
     return (
         <div className="Search-Bar">
-            <input placeholder="Enter A Song, Album or Artist" />
+            <input
+                placeholder="Enter A Song, Album or Artist" />
             <button className="searchButton">Search</button>
         </div>
     )
